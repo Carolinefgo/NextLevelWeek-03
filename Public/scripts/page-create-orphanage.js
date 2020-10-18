d9166f6eee1cb8f8049ef7d61098347e00555851
Carolinefgo <carolinefaragofgo@gmail.com>
2020-10-17 22:52:57 -0300

    att

---
+++ Public/scripts/page-create-orphanage.js
@@ -0,0 +1,86 @@
+//create map
+const map = L.map('mapid').setView([-27.2193107,-49.6540192], 16);
+
+//create and add tilelayer
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',).addTo(map);
+
+
+//create icon
+const icon = L.icon({
+    iconUrl:"./public/images/map-marker.svg",
+    iconSize:[58, 68],
+    iconAnchor:[29, 68],
+    
+
+})
+
+
+let marker;
+
+//create and add marker
+ map.on('click', (event) => {
+     const lat =event.latlng.lat;
+     const lng = event.latlng.lng;
+
+    document.querySelector('[name=lat').value = lat;
+    document.querySelector('[name=lng').value = lng;
+
+    //remove icon 
+    marker && map.removeLayer(marker)
+
+    //add icon layer
+   marker = L.marker([lat, lng], {icon} )
+   .addTo(map)
+}) ;
+
+// add campo de fotos
+function addPhotoField(){
+    //Pegar o container de fotos #images
+    const container = document.querySelector('#images');
+    //pegar o container para duplicar .new-image
+    const fieldsContainer = document.querySelectorAll('.new-upload')
+    //realizar o clone da ultima imagem adicionada.
+    const newFieldContainer = fieldsContainer[fieldsContainer.length -1].cloneNode(true)
+    
+    // verificar se o campo está vazio, se sim, não adicionar ao container de imagens.
+    const input = newFieldContainer.children[0]
+
+    if (input.value == "") {
+        return;
+      }
+
+      input.value = "";
+
+      container.appendChild(newFieldContainer);
+    }
+    
+    function deleteField(event) {
+      const span = event.currentTarget;
+    
+      const fieldsContainer = document.querySelectorAll(".new-upload");
+    
+      if (fieldsContainer.length <= 1) {
+        span.parentNode.children[0].value = "";
+        return;
+      }
+    
+      span.parentNode.remove();
+    }
+    
+    //Select yes or No
+    function toggleSelect(event) {
+      document
+        .querySelectorAll(".button-select button")
+        .forEach((button) => button.classList.remove("active"));
+    
+      const button = event.currentTarget;
+      button.classList.add("active");
+    
+      const input = document.querySelector('[name="open_on_weekends"]');
+      input.value = button.dataset.value;
+    }
+    
+    function validade(event) {
+      // event.preventDefault()
+    }
+
